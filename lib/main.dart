83a94d4a13fa3b7968cb25812462b83394fa1ffc
Selk233 <selkellouti233@gmail.com>
2024-02-02 22:47:08 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:contacttracingprototype/login.dart';
+import 'package:contacttracingprototype/nearby_interface.dart';
+import 'package:contacttracingprototype/registration.dart';
+import 'package:contacttracingprototype/welcome_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print('firebase inited');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomeScreen(),
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        NearbyInterface.id: (context) => NearbyInterface(),
+      },
+    );
+  }
+}
